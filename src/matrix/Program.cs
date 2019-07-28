8e0f98edc5f5831420ff46362cdb8e551092d620
Heath Kang <heath.kang@foxmail.com>
2019-07-28 18:57:12 +0800

    add Matrix method 1) construct 2) Add 3) Transpose

--- src/matrix/Program.cs
+++ src/matrix/Program.cs
@@ -6,7 +6,112 @@ namespace matrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to matrix interface, Please enter dimension of matrix:\n");
+            int dimension = Convert.ToInt32(Console.ReadLine());
+            Matrix matrixA = new Matrix(dimension);
+            Matrix matrixB = new Matrix(dimension);
+
+            Console.WriteLine("Please input matrix:\n");
+            int[] inputArray = new int[matrixA.Length];
+            for (int i = 0; i < matrixA.Length; i ++)
+            {
+                inputArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            matrixA.Add(inputArray);
+            matrixB.Add(inputArray);
+
+            Console.WriteLine("Your matrix input is:\n" );
+            Console.WriteLine(matrixA);
+            Console.WriteLine("Your matrix transpose output is:\n" );
+            matrixB.Transpose();
+            Console.WriteLine(matrixB);
+            
         }
+    }
+
+    class Matrix
+    {
+        int Row;
+        int[,] matrix;
+
+        public Matrix(int dimension)
+        {
+            Row = dimension;
+            matrix =new int[Row,Row];
+        }
+        
+        public int Length
+        {
+            get
+            {
+                return (Row) * (Row);
+            }
+        }
+
+        public int Dimension
+        {
+            get 
+            {
+                return Row;
+            }
+        }
+
+        public void Add(int[] args)
+        {
+            // check args length;
+            if (args.Length > Length)
+            {
+                throw new ArgumentException("Input length is bigger than Matrix length.");
+            }
+            else
+            {
+                // add args to matrix array
+                for(int i = 0;i < args.Length; i++)
+                {
+                    int row = i / Row;
+                    int col = i % Row;
+                    matrix[row,col] = args[i];
+                }
+            }
+        }
+
+        public void Transpose()
+        {
+            // transpose self matrix
+            for (int row = 0; row < Row; row++)
+            {
+                for (int col = 0; col < row; col++)
+                {
+                    int tmp = matrix[row,col];
+                    matrix[row,col] = matrix[col,row];
+                    matrix[col,row] = tmp;
+                }
+            }
+        }
+
+        // public Matrix multi(Matrix anotherMatrix)
+        // {
+        //     // calculate multi another Matrix
+
+        // }
+
+        public override string ToString()
+        {
+            string stringArray = "";
+            for (int row = 0; row < Row; row++)
+            {
+                for (int col = 0; col < Row; col++)
+                {
+                    stringArray = stringArray + matrix[row,col].ToString() + " ";
+                    // enter a line when row = Row
+                    if (col + 1 == Row)
+                    {
+                        stringArray = stringArray + "\n";
+                    }
+                }
+            }
+            return stringArray;
+        }
+
     }
 }
